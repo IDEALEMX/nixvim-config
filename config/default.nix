4a390f288447db576bb65f3febb545e79a2e2d77
IDEALEMX <linxp5acnt@gmail.com>
2025-05-18 00:11:29 -0600

    xd ohio

--- config/default.nix
+++ config/default.nix
@@ -1,11 +1,17 @@
-{
+{ pkgs, ... }: {
 
   # Import all your configuration modules here
   imports = [ ./bufferline.nix ];
 
   # @plugins section
   plugins = {
+
+    # @cosmetic changes
     lualine.enable = true;
+
+    # @latex stuff
+    vimtex.enable = true;
+    cmp-latex-symbols.enable = true;
 
     # lsps, pretty self explainatory
     lsp = {
@@ -20,7 +26,7 @@
 
         # haskell
 	    hls.enable = true;
-      hls.installGhc = true;
+        hls.installGhc = true;
 
 	    # nix
 	    nixd.enable = true;
@@ -39,15 +45,36 @@
         ];
         mapping = {
           "<Tab>" = "cmp.mapping.confirm({ select = true })";
-	  "<C-k>" = "cmp.mapping(function(fallback) cmp.select_prev_item() end, { 'i', 's' })";
-	  "<C-j>" = "cmp.mapping(function(fallback) cmp.select_next_item() end, { 'i', 's' })";
+          "<C-k>" = "cmp.mapping(function(fallback) cmp.select_prev_item() end, { 'i', 's' })";
+          "<C-j>" = "cmp.mapping(function(fallback) cmp.select_next_item() end, { 'i', 's' })";
         };
       };
     };
     
   telescope.enable = true;
-  treesitter.enable = true;
+
+  harpoon = {
+    enable = true;
+    settings = {
+      settings = {
+        save_on_toggle = true;
+        sync_on_ui_close = false;
+      };
+    };
   };
+
+  treesitter.enable = true;
+
+  # @school notes
+
+  # markdown support
+  render-markdown.enable = true;
+
+  # nice headings, checkboxes, etc.
+  headlines.enable = true;
+
+  };
+
 
   # vim's default diagnostic highlights
   diagnostic = {
@@ -67,7 +94,104 @@
   globals.mapleader = " ";
 
   # remaps
-  #keymaps = [{}];
+  keymaps = [
+    # harpoon
+    {
+      mode = "n";
+      key = "<leader>a";
+      action = "<cmd>lua require('harpoon'):list():add()<CR>";
+      options.desc = "Add file to Harpoon";
+    }
+    {
+      mode = "n";
+      key = "<C-e>";
+      action = "<cmd>lua require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())<CR>";
+      options.desc = "Toggle Harpoon quick menu";
+    }
+
+    # telescope
+    {
+      mode = "n";
+      key = "<leader>f";
+      action = "<cmd>Telescope find_files<CR>";
+      options.desc = "Find files with Telescope";
+    }
+
+    # switch buffers
+    {
+        mode = "n";
+        key = "<C-k>";
+        action = ":BufferLineCycleNext<CR>";
+        options.desc = "Go to next buffer";
+    }
+
+    {
+        mode = "t";
+        key = "<C-k>";
+        action = "<C-\\><C-n>:BufferLineCycleNext<CR>";
+        options.desc = "Go to next buffer";
+    }
+
+    {
+        mode = "n";
+        key = "<C-j>";
+        action = ":BufferLineCyclePrev<CR>";
+        options.desc = "Go to previous buffer";
+    }
+
+    {
+        mode = "t";
+        key = "<C-j>";
+        action = "<C-\\><C-n>:BufferLineCyclePrev<CR>";
+        options.desc = "Go to previous buffer";
+    }
+
+    # move around buffers
+    {
+        mode = "n";
+        key = "<C-l>";
+        action = ":BufferLineMoveNext<CR>";
+        options.desc = "move buffer fowards";
+    }
+
+    {
+        mode = "t";
+        key = "<C-l>";
+        action = "<C-\\><C-n>:BufferLineMoveNext<CR>";
+        options.desc = "move buffer fowards";
+    }
+
+    {
+        mode = "n";
+        key = "<C-h>";
+        action = ":BufferLineMovePrev<CR>";
+        options.desc = "move buffer backwards";
+    }
+
+    {
+        mode = "t";
+        key = "<C-h>";
+        action = "<C-\\><C-n>:BufferLineMovePrev<CR>";
+        options.desc = "move buffer backwards";
+    }
+
+    # page up & down centering screen
+
+    {
+        mode = "n";
+        key = "<C-u>";
+        action = "<C-u>zz";
+        options.desc = "page up";
+    }
+
+    {
+        mode = "n";
+        key = "<C-d>";
+        action = "<C-d>zz";
+        options.desc = "page down";
+    }
+
+  ];
 
   extraConfigLua = ''
     -- numbers on the sidelines
@@ -75,11 +199,12 @@
     vim.opt.relativenumber = true
 
     -- how many spaces does the tab add
-    vim.opt.tabstop = 2
-    vim.opt.shiftwidth = 2
+    vim.opt.tabstop = 4
+    vim.opt.shiftwidth = 4
     vim.opt.expandtab = true
 
     -- make the cursor always be a block
     vim.opt.guicursor = "a:block"
   '';
+
 }
---
+++ test.md
@@ -0,0 +1,26 @@
+# teoria de conjuntos
+I really like this kind thing!
+## and this my semi-tittle
+and like this.
+
+``` c 
+int main() {
+    i++;
+};
+```
+
+* thing one
+* thing two
+
+- [x] test1
+- [ ] test2
+- [-] test3
+
+> [!NOTE] 
+> and this is the kind of thing that
+> one must never forget
+
+> [!warning] 
+> yeah
+
+
